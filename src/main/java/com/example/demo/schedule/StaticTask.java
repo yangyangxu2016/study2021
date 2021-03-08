d0ad464ff62ce0a189c2e041a01b34f56ef4de52
xuyangyang <1425809544@qq.com>
2021-03-08 10:19:26 +0800

    feature: 自定义线程池

--- src/main/java/com/example/demo/schedule/StaticTask.java
+++ src/main/java/com/example/demo/schedule/StaticTask.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.concurrent.TimeUnit;
 
 /**
  * 注解方式的优点是简单方便，同样的缺点也明显就是执行周期是固定的不能动态修改
- * 默认是单一线程
+ * 默认是单一线程，任务会排序提交到一个线程的线程池，依次执行
  * author：xuyy
  * date：2021/3/4  4:27 下午
  *
@@ -21,31 +21,30 @@ import java.util.concurrent.TimeUnit;
 @EnableScheduling
 public class StaticTask {
     /**
-     * 1s
+     * 测试定时1s，内部任务小于1s，会每各一秒执行
      */
-    //@Scheduled(cron = "0/1 * * * * ?" )
     @Scheduled(fixedRate = 1000)
     public void doTask1() {
-        log.info("StaticTask_doTask1_start:{}", LocalDateTime.now());
+        log.info("StaticTask_doTask1_start:{}", LocalTime.now());
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.MICROSECONDS.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        log.info("StaticTask_doTask1_end:{}", LocalDateTime.now());
+        //log.info("StaticTask_doTask1_end:{}", LocalTime.now());
     }
 
-    //@Scheduled(cron = "0/3 * * * * ?")
+    /**
+     * 测试定时1s，内部任务大于1s，耗时2s，时间会在任务执行完才会执行下一次定时任务
+     */
     @Scheduled(fixedRate = 1000)
     public void doTask2() {
-        log.info("StaticTask_doTask2_start:{}", LocalDateTime.now());
+        log.info("StaticTask_doTask2_start:{}", LocalTime.now());
         try {
-            TimeUnit.SECONDS.sleep(4);
+            TimeUnit.MICROSECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        log.info("StaticTask_doTask2_end:{}", LocalDateTime.now());
+        //log.info("StaticTask_doTask2_end:{}", LocalTime.now());
     }
-
-
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,129 @@
 package com.example.demo;
 
+import jodd.util.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+@Slf4j
+//@SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+
+    @Test
+    void testThreadPool() throws InterruptedException {
+        //不推荐使用
+        //队列0，每次新建一个线程，大量提交任务并且执行慢，肯产生OOM，无法创建新的线程
+        //Executors.newCachedThreadPool();
+        //固定线程数，但是队列是LinkedBlockingQueue，大量提交任务并且执行慢，可能产生OOM
+        //Executors.newFixedThreadPool(10);
+
+        //int test = test1();
+        //log.info("test:{}", test);
+
+    }
+
+    //private int test1() throws InterruptedException {
+    //    AtomicInteger atomicInteger = new AtomicInteger();
+    //
+    //    //定义合适的核心和最大线程数，合适的队列数量
+    //    ThreadPoolExecutor threadPoolExecutor = new ExtremeThreadPoolExecutor(
+    //            2, 5,
+    //            5l, TimeUnit.SECONDS,
+    //            10
+    //
+    //    );
+    //
+    //    //声明线程池立即达到最大核心线程数
+    //    //threadPoolExecutor.prestartAllCoreThreads();
+    //    //声明在核心线程空闲时，收回核心线程
+    //    //threadPoolExecutor.allowCoreThreadTimeOut(true);
+    //
+    //    IntStream.rangeClosed(1, 20).forEach(i -> {
+    //        try {
+    //            TimeUnit.SECONDS.sleep(1
+    //            );
+    //        } catch (InterruptedException e) {
+    //            e.printStackTrace();
+    //        }
+    //
+    //        int id = atomicInteger.incrementAndGet();
+    //
+    //        try {
+    //            threadPoolExecutor.submit(() -> {
+    //                log.info("{} start ", id);
+    //                try {
+    //                    TimeUnit.SECONDS.sleep(10);
+    //                } catch (InterruptedException e) {
+    //                    e.printStackTrace();
+    //                }
+    //                log.info("{} finished ", id);
+    //            });
+    //        } catch (Exception ex) {
+    //            log.error("id:{},ex:{}", id, ex);
+    //            atomicInteger.decrementAndGet();
+    //        }
+    //    });
+    //
+    //    TimeUnit.SECONDS.sleep(60);
+    //    return atomicInteger.intValue();
+    //}
+    //
+    //
+    //private int test() throws InterruptedException {
+    //    AtomicInteger atomicInteger = new AtomicInteger();
+    //
+    //    //定义合适的核心和最大线程数，合适的队列数量
+    //    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+    //            2, 5,
+    //            5l, TimeUnit.SECONDS,
+    //            new ArrayBlockingQueue<>(10),
+    //            new ThreadFactoryBuilder().withNameFormat("demo-threadPool-%d").get(),
+    //            new ThreadPoolExecutor.AbortPolicy()
+    //    );
+    //
+    //    //声明线程池立即达到最大核心线程数
+    //    threadPoolExecutor.prestartAllCoreThreads();
+    //    //声明在核心线程空闲时，收回核心线程
+    //    threadPoolExecutor.allowCoreThreadTimeOut(true);
+    //
+    //    IntStream.rangeClosed(1, 20).forEach(i -> {
+    //        try {
+    //            TimeUnit.SECONDS.sleep(1
+    //            );
+    //        } catch (InterruptedException e) {
+    //            e.printStackTrace();
+    //        }
+    //
+    //        int id = atomicInteger.incrementAndGet();
+    //
+    //        try {
+    //            threadPoolExecutor.submit(() -> {
+    //                log.info("{} start ", id);
+    //                try {
+    //                    TimeUnit.SECONDS.sleep(10);
+    //                } catch (InterruptedException e) {
+    //                    e.printStackTrace();
+    //                }
+    //                log.info("{} finished ", id);
+    //            });
+    //        } catch (Exception ex) {
+    //            log.error("id:{},ex:{}", id, ex);
+    //            atomicInteger.decrementAndGet();
+    //        }
+    //    });
+    //
+    //    TimeUnit.SECONDS.sleep(60);
+    //    return atomicInteger.intValue();
+    //}
 
 }
