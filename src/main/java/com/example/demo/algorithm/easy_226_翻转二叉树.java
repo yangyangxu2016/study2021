5578b50386609616471bb0b8359945481017778e
xuyangyang <1425809544@qq.com>
2021-12-08 07:49:26 +0800

    easy_226_翻转二叉树.java

--- src/main/java/com/example/demo/algorithm/easy_226_翻转二叉树.java
+++ src/main/java/com/example/demo/algorithm/easy_226_翻转二叉树.java
@@ -1,6 +1,8 @@
 package com.example.demo.algorithm;
 
 
+import org.apache.naming.EjbRef;
+
 /**
  * todo
  * date：2021/12/2  8:52 下午
@@ -10,10 +12,12 @@ package com.example.demo.algorithm;
 public class easy_226_翻转二叉树 {
 
     public TreeNode invertTree(TreeNode root) {
+    //    bsse
         if (root == null) {
-            return null;
+            return root;
         }
-        //交换位置
+
+        //交换左右
         TreeNode tempNode = root.left;
         root.left = root.right;
         root.right = tempNode;
